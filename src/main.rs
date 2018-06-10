767c6832bee7f7f48867659cd6d8e098eafc5944
Shinichi Kogai <kogai0121@gmail.com>
2018-06-10 21:03:32 +0900

    Implement very basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{self, Write};
+use std::process::exit;
+
+pub fn run() {
+    let prompt = "sqlite> ";
+    let mut input_buffer = String::new();
+
+    loop {
+        io::stdout().write_all(&mut prompt.as_bytes()).unwrap();
+        io::stdout().flush().unwrap();
+
+        io::stdin().read_line(&mut input_buffer).unwrap();
+        let cmd = input_buffer.to_owned();
+        match cmd.as_str().trim() {
+            ".exit" => exit(0),
+            x => {
+                println!(r#"Unrecoganized command "{}"."#, x);
+                input_buffer = "".to_string();
+            }
+        };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    io::stdout()
+        .write_all(&mut r#"SQLite version 0.1.0 2018-06-04 19:24:41
+Enter ".help" for usage hints.
+Connected to a transient in-memory database.
+Use ".open FILENAME" to reopen on a persistent database.
+"#.as_bytes())
+        .unwrap();
+    run();
 }
