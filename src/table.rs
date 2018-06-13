f5dabfa72129e39aead0c01ec7ee62d6da00110c
Shinichi Kogai <kogai0121@gmail.com>
2018-06-13 23:24:03 +0900

    Add insert instruction partially

--- src/table.rs
+++ src/table.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use std::mem::transmute;
+use std::ptr;
 
 /*
 id: integer 4bytes
@@ -11,6 +12,11 @@ NOTE: For simplicity, we hardcode a user schema as above.
 const ID_SIZE: usize = 4;
 const USERNAME_SIZE: usize = 32;
 const EMAIL_SIZE: usize = 255;
+const ROW_SIZE: usize = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+const PAGE_SIZE: usize = 4096;
+const ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
+const MAX_PAGE: usize = 100;
+const MAX_ROWS: usize = ROWS_PER_PAGE * MAX_PAGE;
 
 pub struct Row {
   id: u32,
@@ -37,8 +43,8 @@ impl Row {
     }
   }
 
-  pub fn ser(&self) -> [u8; ID_SIZE + USERNAME_SIZE + EMAIL_SIZE] {
-    let mut serialized = [0u8; ID_SIZE + USERNAME_SIZE + EMAIL_SIZE];
+  pub fn ser(&self) -> [u8; ROW_SIZE] {
+    let mut serialized = [0u8; ROW_SIZE];
     let id: [u8; ID_SIZE] = unsafe { transmute(self.id) };
     let username: [u8; USERNAME_SIZE] = unsafe { transmute(self.username) };
     let email: [u8; EMAIL_SIZE] = unsafe { transmute(self.email) };
@@ -46,8 +52,8 @@ impl Row {
     serialized
   }
 
-  pub fn de(source: [u8; ID_SIZE + USERNAME_SIZE + EMAIL_SIZE]) -> Self {
-    let username_offset = ID_SIZE + (USERNAME_SIZE);
+  pub fn de(source: [u8; ROW_SIZE]) -> Self {
+    let username_offset = ID_SIZE + USERNAME_SIZE;
     let id = &source[0..ID_SIZE];
     let username = &source[ID_SIZE..username_offset];
     let email = &source[username_offset..];
@@ -66,6 +72,50 @@ impl Row {
       id,
       username,
       email,
+    }
+  }
+}
+
+type Page = Vec<u8>;
+
+#[derive(Debug)]
+pub struct Table {
+  pages: Vec<Page>,
+  last_row: u32,
+}
+
+impl Table {
+  fn new() -> Self {
+    Table {
+      pages: vec![],
+      last_row: 0,
+    }
+  }
+
+  fn insert(&mut self, row: Row) {
+    let row = row.ser();
+    let row_num = self.last_row as usize;
+    let page_num = row_num / ROWS_PER_PAGE;
+
+    self.last_row += self.last_row;
+
+    let mut page_empty = vec![];
+    page_empty.resize(PAGE_SIZE, 0);
+    let mut page = self.pages.get(page_num).unwrap_or(&page_empty).to_owned();
+
+    let row_offset = row_num % ROWS_PER_PAGE;
+    let byte_offset = row_offset * ROW_SIZE;
+
+    for i in byte_offset..byte_offset + ROW_SIZE {
+      let idx_of_row = i - byte_offset;
+      let el = row.get(idx_of_row).unwrap();
+      page[i] = *el;
+    }
+
+    let page_ptr = self.pages.as_mut_ptr();
+    unsafe {
+      // signal: 11, SIGSEGV: invalid memory reference
+      ptr::write(page_ptr, page);
     }
   }
 }
@@ -106,9 +156,17 @@ mod tests {
       "sample-email@user.com".to_owned(),
     );
     assert_eq!(bytes_of_row, Row::de(bytes_of_row.ser()));
-    assert_eq!(
-      bytes_of_row.ser().len(),
-      ID_SIZE + USERNAME_SIZE + EMAIL_SIZE
-    );
+    assert_eq!(bytes_of_row.ser().len(), ROW_SIZE);
+  }
+
+  #[test]
+  fn test_insert() {
+    let mut table = Table::new();
+    table.insert(Row::new(
+      1u32,
+      "sample-user-name".to_owned(),
+      "sample-email@user.com".to_owned(),
+    ));
+    println!("{:?}", table);
   }
 }
